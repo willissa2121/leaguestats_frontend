48fd5ade43bca02f1b0591d927df833034ea451e
willissa2121 <you@example.com>
2020-03-27 17:08:59 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const handleClick = () => {
+    axios
+      .post("http://localhost:3000/sendData", { value: "peen" })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(e => {
+        console.log("e", e);
+      });
+  };
+  const value = axios.get("http://localhost:3000/sendData").then(data => {
+    console.log(data);
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +31,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleClick}>Button</button>
       </header>
     </div>
   );
